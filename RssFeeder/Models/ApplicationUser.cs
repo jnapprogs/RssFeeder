611235643f4a5509dcf2b12cbef58a4687efdb87
jnapprogs <jnap.progs@gmail.com>
2020-03-16 10:36:34 -0700

    Established a relation between the Rss Links and the Application User

--- RssFeeder/Models/ApplicationUser.cs
+++ RssFeeder/Models/ApplicationUser.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace RssFeeder.Models
 {
     public class ApplicationUser : IdentityUser
     {
-
+        public ICollection<RssLink> RssLinks { get; set; }
     }
 }

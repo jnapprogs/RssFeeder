fcfdd6f8389a73db6b783f5e005cb2b357343a5d
jnapprogs <jnap.progs@gmail.com>
2020-03-16 08:12:11 -0700

    Connected to the database and established the initial identity tables

--- RssFeeder/Data/ApplicationDbContext.cs
+++ RssFeeder/Data/ApplicationDbContext.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RssFeeder.Models;
 
 namespace RssFeeder.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<ApplicationUser> Users { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
